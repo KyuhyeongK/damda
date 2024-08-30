bf0920fb94cd710f80169252c7169f57eec62cc1
kyuhyeong <akcps1tp@naver.com>
2024-08-30 12:43:50 +0900

    refact: 미사용 import 정리

--- bootstrap/src/main/kotlin/com/troy/damda/bootstrap/DamdaApplication.kt
+++ bootstrap/src/main/kotlin/com/troy/damda/bootstrap/DamdaApplication.kt
@@ -3,7 +3,6 @@ package com.troy.damda.bootstrap
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication(scanBasePackages = ["com.troy"])
 @EntityScan(basePackages = ["com.troy"])
