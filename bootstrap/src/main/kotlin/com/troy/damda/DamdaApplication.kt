3df7b80af026b75b3345ece71f106b642ec8ae43
kyuhyeong <akcps1tp@naver.com>
2024-09-03 23:22:39 +0900

    feat: 스프링 프로필 및 로그백 설정 추가

--- bootstrap/src/main/kotlin/com/troy/damda/DamdaApplication.kt
+++ bootstrap/src/main/kotlin/com/troy/damda/DamdaApplication.kt
@@ -1,11 +1,9 @@
 package com.troy.damda
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication(scanBasePackages = ["com.troy"])
-//@EntityScan(basePackages = ["com.troy"])
 class DamdaApplication
 
 fun main(args: Array<String>) {
