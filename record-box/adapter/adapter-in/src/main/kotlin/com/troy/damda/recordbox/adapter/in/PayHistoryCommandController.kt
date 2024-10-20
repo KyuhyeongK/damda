775a9463269018d97e4fcb7cee5caa4e10a4c29d
kyuhyeong <akcps1tp@naver.com>
2024-10-21 00:19:38 +0900

    refact: 납부내역 create api 수정
    - eventId를 pathVariable로 받기 위해 requestBody와 in port 클래스 분리

--- record-box/adapter/adapter-in/src/main/kotlin/com/troy/damda/recordbox/adapter/in/PayHistoryCommandController.kt
+++ record-box/adapter/adapter-in/src/main/kotlin/com/troy/damda/recordbox/adapter/in/PayHistoryCommandController.kt
@@ -4,28 +4,36 @@ import com.troy.damda.DamdaResponse
 import com.troy.damda.auth.application.port.`in`.UserMgmtNo
 import com.troy.damda.logger
 import com.troy.damda.ok
+import com.troy.damda.recordbox.application.domain.PayType
 import com.troy.damda.recordbox.application.port.`in`.CreatePayHistoryUseCase
 import com.troy.damda.recordbox.application.port.`in`.CreatePayHistoryUseCase.CreatePayHistoryRequest
 import com.troy.damda.recordbox.application.port.`in`.CreatePayHistoryUseCase.CreatePayHistoryResponse
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.math.BigDecimal
 
 @RestController
-@RequestMapping("/damda/v1/record-box/pay-history")
+@RequestMapping("/damda/v1/record-box")
 class PayHistoryCommandController(
     private val createPayHistoryUseCase: CreatePayHistoryUseCase,
 ) {
 
     private val log = logger()
 
-    @PostMapping
+    @PostMapping("/{eventId}/pay-history")
     fun createPayHistory(
         @UserMgmtNo userMgmtNo: Long,
-        @RequestBody request: CreatePayHistoryRequest
+        @PathVariable eventId: Long,
+        @RequestBody request: CreatePayHistoryApiRequest
     ): DamdaResponse<CreatePayHistoryResponse> {
         log.debug("createPayHistory :: req => {}", request)
-        return ok(createPayHistoryUseCase.createPayHistory(userMgmtNo, request))
+        return ok(createPayHistoryUseCase.createPayHistory(
+            userMgmtNo,
+            CreatePayHistoryRequest(request.type, request.payAmount, eventId))
+        )
     }
+
+    data class CreatePayHistoryApiRequest(
+        val type: PayType,
+        val payAmount: BigDecimal,
+    )
 }
